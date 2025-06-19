334e114b04120df78e1b843b66ad40f9ad5fcac3
Wannes <vandeputte.wannes@gmail.com>
2025-06-19 19:15:43 +0200

    Vite build changes

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,15 @@ export default defineConfig(() => {
         "Cross-Origin-Embedder-Policy": "require-corp",
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+          manualChunks: undefined,
+        },
+      },
+    },
   };
 });
