a5abad57c03cd2550aa26ec542fc4070cae30fa2
Wannes <vandeputte.wannes@gmail.com>
2025-08-21 10:14:14 +0200

    Remove svg_collision_detector

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,14 +4,12 @@ mod utils;
 mod lbf;
 mod logger;
 mod sparrow;
-mod svg_collision;
 mod svg_exporter;
 mod terminator;
 
 pub use lbf::run_lbf;
 pub use logger::init_logger;
 pub use sparrow::run_sparrow;
-pub use svg_collision::{svg_collision, svg_collision_test};
 pub use svg_exporter::WasmSvgExporter;
 pub use terminator::WasmTerminator;
 pub use wasm_bindgen_rayon::init_thread_pool;
